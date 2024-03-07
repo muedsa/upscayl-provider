1cfd20d7233d481ea4ea05d8faee514bb174e095
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-03-07 16:58:03 +0800

    fix: not exist db column hash

--- src/main/kotlin/com/muedsa/upscayl/dao/ImageUrlAliasDAO.kt
+++ src/main/kotlin/com/muedsa/upscayl/dao/ImageUrlAliasDAO.kt
@@ -1,8 +1,10 @@
 package com.muedsa.upscayl.dao
 
+import com.muedsa.upscayl.dao.ImageUrlAliasDAO.ImageUrlAliasTable
 import com.muedsa.upscayl.model.ImageUrlAlias
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -10,7 +12,7 @@ class ImageUrlAliasDAO(private val database: Database) {
     object ImageUrlAliasTable : Table("image_url_alias") {
         val id = integer("id").autoIncrement()
         val url = varchar("url", length = 512).index(isUnique = true)
-        val hash = varchar("name", length = 64).index() // sha-256
+        val hash = varchar("hash", length = 64).index() // sha-256
 
         override val primaryKey = PrimaryKey(id)
     }
@@ -26,7 +28,7 @@ class ImageUrlAliasDAO(private val database: Database) {
 
     suspend fun getByUrl(url: String): ImageUrlAlias? {
         return dbQuery {
-            ImageUrlAliasTable.select { ImageUrlAliasTable.url eq url }
+            ImageUrlAliasTable.selectAll().where { ImageUrlAliasTable.url eq url }
                 .map {
                     ImageUrlAlias(
                         it[ImageUrlAliasTable.url],
@@ -39,7 +41,7 @@ class ImageUrlAliasDAO(private val database: Database) {
 
     suspend fun getByHash(hash: String): ImageUrlAlias? {
         return dbQuery {
-            ImageUrlAliasTable.select { ImageUrlAliasTable.hash eq hash }
+            ImageUrlAliasTable.selectAll().where { ImageUrlAliasTable.hash eq hash }
                 .map {
                     ImageUrlAlias(
                         it[ImageUrlAliasTable.url],
